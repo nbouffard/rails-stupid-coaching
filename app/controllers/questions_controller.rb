4cd9e950234285436a88d7072f08cb56bca8f695
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-11 14:47:05 +0200

    fixed styling issue in #answer

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,13 +4,8 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:answer]
-    case @question
-    when 'I am going to work'
-      @answer = 'Great'
-    when @question.end_with?('?')
-      @answer = 'I am going to work'
-    else
-      @answer = "I don't care, get dressed and go to work!"
-    end
+    @answer = "I don't care, get dressed and go to work!"
+    @answer = 'Great' if @question == 'I am going to work'
+    @answer = 'Silly question, get dressed and go to work!' if @question.end_with?('?') == true
   end
 end
