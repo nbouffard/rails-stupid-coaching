31a8ac594a7271518fe29073566ca800669b3515
nicola bouffard <nicola.bouffard855@gmail.com>
2023-05-11 14:25:18 +0200

    added question and answer to answer page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,14 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    @question = params[:answer]
+    case @question
+    when 'I am going to work'
+      @answer = 'Great'
+    when @question.end_with?('?')
+      @answer = 'I am going to work'
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
